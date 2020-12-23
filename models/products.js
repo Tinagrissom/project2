e7cbe1ce9406059f45d302adb4fdb5e7b52dc9c7
Tina <Tina@macusers-MBP-4.attlocal.net>
2020-12-23 11:34:27 -0600

    index page created, seed route created, and both showing in browser

---
+++ models/products.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: String,
+  img: String,
+  genre: String,
+  price: {type: Number, min: 0},
+  qty: {type: Number, min: 0},
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
+const Product = require('./models/products.js')
 //setting db as our connection
 const db = mongoose.connection
 require('dotenv').config()
@@ -33,9 +34,36 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/homepage', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render(
+      'index.ejs',
+      {
+        products: allProducts
+      }
+    )
+  })
 })
+
+app.get('/homepage/seed', (req, res) => {
+  Product.create(
+    [
+      {
+        title: 'Here You Come Again',
+        artist: 'Dolly Parton',
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Hereyoucomeagain.jpg/220px-Hereyoucomeagain.jpg',
+        genre: 'Country',
+        price: 25,
+        qty: 4,
+      }
+    ],
+    (error, data) => {
+      res.redirect('/homepage')
+    }
+  )
+})
+
+
 app.listen(PORT, () => {
   console.log('Listening on port: ', PORT)
 })
