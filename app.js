75ca3411dc621679d050f8796220cec1524a60f4
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-01-06 13:00:40 -0600

    added mobile styling

---
+++ app.js
@@ -0,0 +1,14 @@
+// $(() => {
+//
+//   $(document).ready(function() {
+//     console.log("ready");
+//     var delay = 300;
+//
+//     $("#list-builder").delay(delay).fadeIn("fast", () => {
+//       $("#popup-box").fadeIn("fast", () => {})
+//     })
+//     $("popup-close").click(() => {
+//       $("#list-builder, #popup-box").hide();
+//     })
+//   })
+// })
